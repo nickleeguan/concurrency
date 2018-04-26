d39fefc050d1433239b8cd30766d3f054fa58333
醒白 <469522336@qq.com>
2018-04-26 10:20:53 +0800

    线程安全策略2

---
+++ src/main/java/com/icode/concurrency/example/concurrent/CopyOnWriteSetExample.java
@@ -0,0 +1,46 @@
+package com.icode.concurrency.example.concurrent;
+
+import com.icode.concurrency.annoations.ThreadSafe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+import java.util.concurrent.*;
+
+@ThreadSafe
+public class CopyOnWriteSetExample {
+    private static final Logger log = LoggerFactory.getLogger(CopyOnWriteSetExample.class);
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    private static Set<Integer> set = new CopyOnWriteArraySet<>();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size:{}", set.size());
+    }
+
+    private static void update(int i) {
+        set.add(i);
+    }
+}
